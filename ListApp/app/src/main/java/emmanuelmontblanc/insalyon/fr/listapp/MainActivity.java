58b0dbbc75ca6f337e8b4cf88ea565a0ec04a524
unknown <emmanuel.montblanc@insa-lyon.fr>
2022-05-03 10:44:31 +0300

    list app

---
+++ ListApp/app/src/main/java/emmanuelmontblanc/insalyon/fr/listapp/MainActivity.java
@@ -0,0 +1,42 @@
+package emmanuelmontblanc.insalyon.fr.listapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        myListView = (ListView) findViewById(R.id.MyListView);
+        items = getResources().getStringArray(R.array.items);
+        prices = getResources().getStringArray(R.array.prices);
+        descriptions = getResources().getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
+        myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("emmanuelmontblanc.insalyon.fr.INDEX", i);
+                startActivity(showDetailActivity);
+            }
+        });
+
+    }
+}
