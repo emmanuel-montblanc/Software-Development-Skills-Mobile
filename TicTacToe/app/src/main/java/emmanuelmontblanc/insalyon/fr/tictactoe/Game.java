303de784ce03026c0213d4ff948542f99f4f581a
Emmanuel MONTBLANC <emmanuel.montblanc@insa-lyon.fr>
2022-05-06 12:34:29 +0300

    Created the grid for the game

--- TicTacToe/app/src/main/java/emmanuelmontblanc/insalyon/fr/tictactoe/Game.java
+++ TicTacToe/app/src/main/java/emmanuelmontblanc/insalyon/fr/tictactoe/Game.java
@@ -1,12 +1,20 @@
 package emmanuelmontblanc.insalyon.fr.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.Arrays;
+
 public class Game extends AppCompatActivity {
+
+    ItemAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +33,18 @@ public class Game extends AppCompatActivity {
                 gametypeTextView.setText("Player vs IA");
             }
         }
+
+
+//        String[] data = {"1", "2", "3", "4", "5", "6", "7", "8", "9"};
+        int[] data = new int[9];
+        Arrays.fill(data, R.drawable.circle);
+
+        RecyclerView recyclerView = findViewById(R.id.gameRecyclerView);
+//        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setLayoutManager(new GridLayoutManager(this, 3));
+        recyclerView.setAdapter(new ItemAdapter(data));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        recyclerView.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.HORIZONTAL));
 
     }
 }
