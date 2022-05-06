1f72627b20da2fa15635b2b66ba2988f39bf9453
Emmanuel MONTBLANC <emmanuel.montblanc@insa-lyon.fr>
2022-05-06 15:18:40 +0300

    Detects the end of the game

--- TicTacToe/app/src/main/java/emmanuelmontblanc/insalyon/fr/tictactoe/Game.java
+++ TicTacToe/app/src/main/java/emmanuelmontblanc/insalyon/fr/tictactoe/Game.java
@@ -8,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -23,7 +24,8 @@ public class Game extends AppCompatActivity implements GameAdapter.ItemClickList
     TextView turnTextView;
     TextView gametypeTextView;
     int turn;
-    int[] gameState;
+    int[][] gameState;
+    int end;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +35,9 @@ public class Game extends AppCompatActivity implements GameAdapter.ItemClickList
         gametypeTextView = (TextView) findViewById(R.id.gametypeTextView);
         turnTextView = (TextView) findViewById(R.id.turnTextView);
 
-        gameState = new int[9];
-        Arrays.fill(gameState, -1);
+        gameState = new int[][] { {-1,-1,-1}, {-1,-1,-1}, {-1,-1,-1}};
         turn = 0;
+        end = -1;
         turnTextView.setText("Player 1");
 
         Intent in  = getIntent();
@@ -65,23 +67,101 @@ public class Game extends AppCompatActivity implements GameAdapter.ItemClickList
 
     @Override
     public void onItemClick(View view, int position) {
-        ImageView caseImageView = view.findViewById(R.id.caseImageView);
 
-        if(gameState[position] == -1) {
-            if (turn == 0) {
-                caseImageView.setImageResource(R.drawable.cross);
-                gameState[position] = 0;
-                turn = 1;
-                turnTextView.setText("Player 2");
+        if(end == -1) {
+            ImageView caseImageView = view.findViewById(R.id.caseImageView);
+
+            int col = position / 3;
+            int row = position % 3;
+
+            if (gameState[col][row] == -1) {
+                if (turn == 0) {
+                    caseImageView.setImageResource(R.drawable.cross);
+                    gameState[col][row] = 0;
+                } else {
+                    caseImageView.setImageResource(R.drawable.circle);
+                    gameState[col][row] = 1;
+                }
+
+                end = checkEnd(col, row, turn);
+
+                if (end == -1) {
+                    if (turn == 0) {
+                        turn = 1;
+                        turnTextView.setText("Player 2");
+                    } else {
+                        turn = 0;
+                        turnTextView.setText("Player 1");
+                    }
+                } else {
+                    Toast.makeText(this, "end !" + end, Toast.LENGTH_SHORT).show();
+                }
+
             } else {
-                caseImageView.setImageResource(R.drawable.circle);
-                gameState[position] = 1;
-                turn = 0;
-                turnTextView.setText("Player 1");
+                Toast.makeText(this, "Invalid play !", Toast.LENGTH_SHORT).show();
             }
-        } else {
-            Toast.makeText(this, "Invalid play !", Toast.LENGTH_SHORT).show();
         }
+    }
+
+    public int checkEnd(int col, int row, int turn){
+
+        int end = -1;
+
+        // Checks draw
+        for(int i = 0; i < 3; i++){
+            for(int j = 0; j < 3; j++){
+                if(gameState[i][j] == -1){
+                    break;
+                }
+
+                if(i == 2 & j == 2){
+                    end = 2;
+                }
+            }
+        }
+
+        // Checks col
+        for(int i = 0; i < 3; i++){
+            if(gameState[col][i] != turn)
+                break;
+            if(i == 2){
+                end = turn;
+            }
+        }
+
+        // Checks row
+        for(int i = 0; i < 3; i++){
+            if(gameState[i][row] != turn)
+                break;
+            if(i == 2){
+                end = turn;
+            }
+        }
+
+        // Checks diag
+        if(row == col){
+            //we're on a diagonal
+            for(int i = 0; i < 3; i++){
+                if(gameState[i][i] != turn)
+                    break;
+                if(i == 2){
+                    end = turn;
+                }
+            }
+        }
+
+        // Checks anti diag
+        if(row + col == 2){
+            for(int i = 0; i < 3; i++){
+                if(gameState[i][2-i] != turn)
+                    break;
+                if(i == 2){
+                    end = turn;
+                }
+            }
+        }
+
+        return end;
     }
 
 }
