8d29be8fc50c4bb925b9c8b6b9c2083273eee1cc
Emmanuel MONTBLANC <emmanuel.montblanc@insa-lyon.fr>
2022-05-07 10:28:55 +0300

    added a button to return to the main menu to the VictoryScreen

--- TicTacToe/app/src/main/java/emmanuelmontblanc/insalyon/fr/tictactoe/VictoryScreen.java
+++ TicTacToe/app/src/main/java/emmanuelmontblanc/insalyon/fr/tictactoe/VictoryScreen.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class VictoryScreen extends AppCompatActivity {
@@ -36,6 +38,15 @@ public class VictoryScreen extends AppCompatActivity {
         }
         winnerTextView.setText(winnerString);
 
-
+        // Button to return to the main menu
+        Button mainMenuBtn = findViewById(R.id.mainMenuBtn);
+        mainMenuBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(getApplicationContext(), MainActivity.class);
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+        });
     }
 }
